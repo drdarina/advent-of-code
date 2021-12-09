a3d9d18ba52d8049f4ca3a6bf8c9699b07e824db
Darina Goldin <darinagoldin@googlemail.com>
2021-12-09 12:56:04 +0100

    Day 6 and 7 part 1

---
+++ 2021/6.py
@@ -0,0 +1,26 @@
+test = True
+if test:
+    arr = [3,4,3,1,2]
+else:
+    arr = [3,1,4,2,1,1,1,1,1,1,1,4,1,4,1,2,1,1,2,1,3,4,5,1,1,4,1,3,3,1,1,1,1,3,3,1,3,3,1,5,5,1,1,3,1,1,2,1,1,1,3,1,4,3,2,1,4,3,3,1,1,1,1,5,1,4,1,1,1,4,1,4,4,1,5,1,1,4,5,1,1,2,1,1,1,4,1,2,1,1,1,1,1,1,5,1,3,1,1,4,4,1,1,5,1,2,1,1,1,1,5,1,3,1,1,1,2,2,1,4,1,3,1,4,1,2,1,1,1,1,1,3,2,5,4,4,1,3,2,1,4,1,3,1,1,1,2,1,1,5,1,2,1,1,1,2,1,4,3,1,1,1,4,1,1,1,1,1,2,2,1,1,5,1,1,3,1,2,5,5,1,4,1,1,1,1,1,2,1,1,1,1,4,5,1,1,1,1,1,1,1,1,1,3,4,4,1,1,4,1,3,4,1,5,4,2,5,1,2,1,1,1,1,1,1,4,3,2,1,1,3,2,5,2,5,5,1,3,1,2,1,1,1,1,1,1,1,1,1,3,1,1,1,3,1,4,1,4,2,1,3,4,1,1,1,2,3,1,1,1,4,1,2,5,1,2,1,5,1,1,2,1,2,1,1,1,1,4,3,4,1,5,5,4,1,1,5,2,1,3]
+
+def substract_produce(i):
+    if i == 0:
+        return 6, True
+    else:
+        return i - 1, False
+
+# brute force but I have a migraine
+for i in range(80):
+    new_arr = []
+    for a in arr:
+        reduced, fish = substract_produce(a)
+        new_arr.append(reduced)
+        if fish:
+            new_arr.append(8)
+    arr = new_arr[:]
+
+print(len(arr))
+
+# this will not work for the second part of the puzzle because the array will become too long
+# we'd need to simulate each fish individually then multiply by the number of fish starting in that position
