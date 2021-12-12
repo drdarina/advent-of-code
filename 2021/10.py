8f4435fb030ca33e89703b4d10f30dae2030e841
Darina Goldin <darina@dojomadness.com>
2021-12-12 23:48:01 +0100

    Day 12 wip

--- 2021/10.py
+++ 2021/10.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 test = True
 with open(f'data/input10{"_test" if test else ""}.txt', 'r') as f:
     data = f.readlines()
@@ -8,31 +10,54 @@ value_lookup = {')': 3,
                 '>': 25137
                 }
 
-data = [i.split()[0] for i in data]
+data = [list(i.split()[0]) for i in data]
 
 opening = ['(', '[', '{', '<']
 closing = [')', ']', '}', '>']
 pairs = dict(zip(opening, closing))
+center = [i+j for i, j in zip(opening, closing)]
 
 
-def check_row(row):
-    # find the first occurrence of a closing bracket at index i
-    # from there go outward, checking if i-1, i match, i-2, i+1 match, etc, until you arrive at the next opening bracket
-    # restart the process
-    for idx, c in enumerate(row):
-        while c in opening:
-            continue
-    # idx is now the index of the first closing bracket
-    left = c[:idx]
-    right = c[idx:2*idx]
-    left = [pairs[i] for i in left][::-1]
-    print(left, right)
-    if left == right:
-        return True
-    # todo - keep checking the array. option b would be to immediately find pairs like '()' and start looking outward
-    return False
-
-
-
+not_corrupted = []
+unmatched_sum = 0
+corrupted = False
 for row in data:
-    check_row(row)
+    o = []
+    # skip the first closing brackets
+    for idx, c in enumerate(row):
+        if c in opening:
+            break
+    row = row[idx:]
+    for idx, c in enumerate(row):
+        if c in opening:
+            o.append(c)
+        else:
+            if c == pairs[o[-1]]:
+                o.pop(-1)
+            else:
+                unmatched_sum += (value_lookup[c])
+                corrupted = True
+                break
+
+    if not corrupted:
+        not_corrupted.append(row)
+    corrupted = False
+
+
+print(unmatched_sum)
+
+# part 2
+
+value_lookup = zip([')', ']', '}', '>'], range(1,5))
+
+#todo - hadn't read the puzzle correctly
+scores = []
+for row in not_corrupted:
+    scores.append(0)
+    for bracket in opening:
+        n_open = row.count(bracket)
+        n_closed = row.count(pairs[bracket])
+        scores[-1]+=(n_open - n_closed) * value_lookup[pairs[bracket]]
+
+print(scores)
+print(int(np.median(scores)))
---
+++ 2021/12.py
@@ -0,0 +1,36 @@
+test = True
+with open(f'data/input12{"_test" if test else ""}.txt', 'r') as f:
+    data = f.readlines()
+
+
+#Your goal is to find the number of distinct paths that start at start, end at end, and don't visit small caves more
+# than once. There are two types of caves: big caves (written in uppercase, like A) and small caves (written in
+# lowercase, like b). It would be a waste of time to visit any small cave more than once, but big caves are large
+# enough that it might be worth visiting them multiple times. So, all paths you find should visit small caves at
+# most once, and can visit big caves any number of times.
+
+import networkx as nx
+import numpy as np
+import matplotlib.pyplot as plt
+g = nx.Graph()
+
+data = [i.split()[0].split('-') for i in data]
+
+for row in data:
+    g.add_edge(*row)
+
+
+paths = {}
+node = 'start'
+paths[node] = {}
+
+for edge in g.edges(node):
+    paths[node][]
+    paths[node] = {}
+    edges = g.edges(node)
+    for edge in edges:
+        paths[node][edge[1]] = {}
+
+paths = []
+print(g.edges('start'))
+print(g.edges('A'))
