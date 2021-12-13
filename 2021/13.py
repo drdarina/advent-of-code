27514eb897ffc782b82c550258baa06c77ac9723
Darina Goldin <darinagoldin@googlemail.com>
2021-12-13 16:26:36 +0100

    Day 13 wip

--- 2021/13.py
+++ 2021/13.py
@@ -1,32 +1,50 @@
 import numpy as np
 
-test = True
+test = False
 with open(f'data/input13{"_test" if test else ""}.txt', 'r') as f:
     data = f.readlines()
 
 coords = [[int(j) for j in i.split()[0].split(',')] for i in data if not i.startswith('fold') and i!='\n']
-folds = [i.split()[-1] for i in data if i.startswith('fold')]
+folds = [i.split()[-1].split('=') for i in data if i.startswith('fold')]
+folds = [(i, int(j)) for (i, j) in folds]
 
 arr = np.zeros((max([i[0] for i in coords])+1, max([i[1] for i in coords])+1))
 for pair in coords:
     arr[pair[0], pair[1]] = 1
 arr = arr.T
-print(arr)
 
 def fold_up(arr, x):
     # split into two pars
     arr1 = arr[:x, :]
     arr2 = arr[x+1:, :]
+    if arr1.shape[0] < arr2.shape[0]:
+        arr1 = np.vstack([np.zeros((arr2.shape[0] - arr1.shape[0], arr1.shape[1])), arr1])
+    else:
+        arr2 = np.vstack([np.zeros((arr1.shape[0] - arr2.shape[0], arr1.shape[1])), arr2])
     # reverse second array for "folding"
-    arr2 = arr2[::-1,:]
-    # todo: sum arr1 + arr2, padding zeroes in the smaller array
-
-
+    arr2 = arr2[::-1, :]
+    return arr1 + arr2
 
 
 def fold_left(arr, y):
     arr1 = arr[:, :y]
     arr2 = arr[:, y + 1:]
+    if arr1.shape[1] < arr2.shape[1]:
+        arr1 = np.hstack([arr1, np.zeros((arr1.shape[0], arr2.shape[1] - arr1.shape[1]))])
+    else:
+        arr2 = np.hstack([arr2, np.zeros((arr1.shape[0], arr1.shape[1] - arr2.shape[1]))])
+    # reverse second array for "folding"
+    arr2 = arr2[:, ::1]
+    return arr1 + arr2
 
 
-fold_up(arr, 7)
+for direction, value in folds[:1]:
+    print(direction, value)
+    if direction == 'y':
+        arr = fold_up(arr, value)
+    else:
+        arr = fold_left(arr, value)
+
+print(np.sum(np.where(arr > 0, 1, 0)))
+
+print(arr)
